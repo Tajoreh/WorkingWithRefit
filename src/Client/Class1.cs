705ab282e0f89a0f2b2636145464013e6b07d21a
tajoreh <tajoreh@gmail.com>
2024-02-13 13:44:36 +0330

    init

---
+++ src/Client/Class1.cs
@@ -0,0 +1,13 @@
+﻿using Refit;
+namespace Client;
+
+public class DataDto
+{
+    public string Value { get; set; }
+}
+
+public interface IWebAppApi
+{
+    [Post("/verify")]
+    Task<string> SendData([AliasAs("data")] DataDto data);
+}
---
+++ src/Client/Program.cs
@@ -0,0 +1,18 @@
+﻿using Refit;
+using System.Text.Json.Serialization;
+using System.Text.Json;
+using Client;
+
+var webAppApi = RestService.For<IWebAppApi>("https://localhost:7292", new RefitSettings
+{
+    ContentSerializer = new SystemTextJsonContentSerializer(new JsonSerializerOptions { DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull }),
+    UrlParameterFormatter = new DefaultUrlParameterFormatter()
+});
+
+var dataToSend = new DataDto()
+{
+    Value = "Value",
+};
+var response = await webAppApi.SendData(dataToSend);
+
+Console.WriteLine(response);
